0d95eb521bea3b5d9906999bb3a75fd5bf926f92
kenroly <82363621+kenroly@users.noreply.github.com>
2024-01-23 18:56:19 +0700

    Change left side to js

--- student/assets/js/index.js
+++ student/assets/js/index.js
@@ -1,23 +1,31 @@
-import { getLocalStorage } from '../../../assets/js/tools.js';
-import { lang_count } from '../../../assets/lang/lang_count.js';
+// import { getLocalStorage } from '../../../assets/js/tools.js';
+// import { lang_count } from '../../../assets/lang/lang_count.js';
 
-let globalLogData = getLocalStorage("GLOBAL_LOG_DATA");
-const lang = globalLogData.lang;
-let global_lang = '';
+// let globalLogData = getLocalStorage("GLOBAL_LOG_DATA");
+// const lang = globalLogData.lang;
+// let global_lang = '';
 
-for (let i of lang_count) {
-    if (lang == i) {
-        global_lang = i; 
-        break;
-    }
-}
+// for (let i of lang_count) {
+//     if (lang == i) {
+//         global_lang = i; 
+//         break;
+//     }
+// }
 
-const lang_path = `../../../assets/lang/${global_lang}/lang_std/lang.json`;
-console.log(lang_path);
-import(lang_path)
-    .then(module => {
-        const data = module.default; 
-        console.log(data);
-    })
+// const lang_path = `../../../assets/lang/${global_lang}/lang_std/lang.json`;
+// console.log(lang_path);
+// import(lang_path)
+//     .then(module => {
+//         const data = module.default; 
+//         console.log(data);
+//     })
 
+import { renderLeftSide } from './left-side.js';
+console.log(123);
+const renderIndex = (container) => {
+    const leftSide = renderLeftSide(container);
+    container.appendChild(leftSide);
+};
 
+let container = document.getElementById("container");
+renderIndex(container);
---
+++ student/assets/js/left-side.js
@@ -0,0 +1,87 @@
+import { global_lang } from "../../../config/lang.js";
+const leftSideData = [
+    {
+        title: {
+            "vi": "Phòng học hôm nay",
+            "en": "Today's classroom"
+        },
+        default_selected: true,
+        href_part: "today",
+        widget_icon: "bxs-widget",
+    },
+    {
+        title: {
+            "vi": "Bài tập chưa làm",
+            "en": "Unfinished exercises",
+        },
+        default_selected: false,
+        href_part: "ex",
+        widget_icon: "bx-note",
+    },
+    {
+        title: {
+            "vi": "Tài liệu chưa đọc",
+            "en": "Unread documents",
+        },
+        default_selected: false,
+        href_part: "doc",
+        widget_icon: "bx-folder-minus",
+    },
+    {
+        title: {
+            "vi": "Bài giảng chưa xem",
+            "en": "Unwatched lectures",
+        },
+        default_selected: false,
+        href_part: "video",
+        widget_icon: "bx-play-circle",
+    },
+    {
+        title: {
+            "vi": "Thành tích",
+            "en": "Achievements",
+        },
+        default_selected: false,
+        href_part: "achievement",
+        widget_icon: "bx-trophy",
+    },
+];
+export const renderLeftSide = (container) => {
+    let leftSide = document.createElement("div");
+    leftSide.setAttribute("id", "left-side");
+    leftSide.classList.add("left-side-menu");
+
+    for (let i of leftSideData) {
+        const widgetIcon = document.createElement("i");
+        widgetIcon.classList.add("bx");
+        widgetIcon.classList.add(i.widget_icon);
+
+        const title = document.createElement("p");
+        title.innerHTML = i.title[global_lang];
+
+        const checkIcon = document.createElement("i");
+        checkIcon.classList.add("bx", "bx-check");
+
+        const flexBox = document.createElement("div");
+        flexBox.classList.add("aic", "df")
+        flexBox.appendChild(widgetIcon);
+        flexBox.appendChild(title);
+        flexBox.appendChild(checkIcon);
+
+        const objectDiv = document.createElement("div");
+        objectDiv.classList.add("object-div");
+        objectDiv.appendChild(flexBox);
+
+        const leftSideItem = document.createElement("a");
+        leftSideItem.classList.add("object");
+        if (i.default_selected) {
+            leftSideItem.classList.add("left-menu-selected");
+        }
+        leftSideItem.setAttribute("href", `#${i.href_part}`);
+
+        leftSideItem.appendChild(objectDiv);
+        leftSide.appendChild(leftSideItem);
+    }
+
+    return leftSide;
+}
